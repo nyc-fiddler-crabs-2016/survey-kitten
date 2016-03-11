2c6e0329ac42b1d13f5f2c69aa207050a34878ac
petestrat & Matthew Oppenheimer & Matthew Baquerizo & Andrew Kim <pair+Petestrat.mattopps.mlbaquerizo.theandrewykim@devbootcamp.com>
2016-03-11 15:27:53 -0500

    Databse works for real this time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,26 @@
-User.create(username:"Bob", password:"password", email:"bob@bob.com")
-User.create(username:"Big bob", password:"password", email:"bob@bob.com")
+bob = User.create(username:"Bob", password:"password", email:"bob@bob.com")
+big_bob = User.create(username:"Big bob", password:"password", email:"bob@bob.com")
 
 
 s1 = Survey.create(name:"Survey 1", category:"programming",creator_id:1)
+
+  s1q1 = s1.questions.create(question_text:"What is the meaning of life")
+    s1q1c1 = s1.questions.first.choices.create(choice_text:"Nothing")
+    s1q1c2 = s1.questions.first.choices.create(choice_text:"To procreate")
+
+  s1q2 = s1.questions.create(question_text:"How big do you think Big Bob is?")
+    s1q2c1 = s1.questions.last.choices.create(choice_text:"Not big at all")
+    s1q2c2 = s1.questions.last.choices.create(choice_text:"Quite large")
+
 s2 = Survey.create(name:"Survey 2", category:"hip hop dance",creator_id:2)
 
-
-s1.questions.create(question_text:"What is the meaning of life")
-s1.questions.create(question_text:"How big do you think Big Bob is?")
-
-s2.questions.create(question_text:"Why do you suck?")
-s2.questions.create(question_text:"Why?")
-
-
-s1.questions.first.choices.create(choice_text:"Nothing")
-s1.questions.first.choices.create(choice_text:"To procreate")
-
-s1.questions.last.choices.create(choice_text:"Not big at all")
-s1.questions.last.choices.create(choice_text:"Quite large")
-
+  s2q1 = s2.questions.create(question_text:"Why do you suck?")
+  s2q2 = s2.questions.create(question_text:"Why?")
 
 s2.questions.first.choices.create(choice_text:"idk")
 s2.questions.first.choices.create(choice_text:"Nothing")
-
 s2.questions.last.choices.create(choice_text:"idk")
 s2.questions.last.choices.create(choice_text:"you suck")
 
+Selection.create(taker_id: 2, question_id: 1, choice_id: 1, survey_id: 1)
 
